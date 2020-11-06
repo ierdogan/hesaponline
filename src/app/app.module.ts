07cc4b988581a1def03513a1cf2dcbfe4eb7acab
İlker Erdoğan <ierdogan@hotmail.com>
2020-11-06 18:18:31 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
